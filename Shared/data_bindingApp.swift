b67b3c05a67aedc21c4b9f56d5f0e4d767f4759c
berry <berry3085@kimo.com>
2022-01-26 14:13:45 +0800

    Initial Commit

---
+++ Shared/data_bindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  data_bindingApp.swift
+//  Shared
+//
+//  Created by 沈秉叡 on 2022/1/26.
+//
+
+import SwiftUI
+
+@main
+struct data_bindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
